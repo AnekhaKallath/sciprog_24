c03da2c9a47f121b6401cca567602bc1966ff3ff
Anekha Kallath <anekha.kallath@ucdconnect.ie>
2024-11-01 02:11:49 +0000

    Add files to practical01 folder

---
+++ practical01/printing.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(void) {
+   int accountno=1;
+   float sub_total=1234.56, total=7890.12;
+
+   printf("Account: %d Subtotal: %.2f Total: %.2f\n",accountno,
+        sub_total,total);
+
+  return 0;
+}
+
+
+
